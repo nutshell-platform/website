5081dd78d0685ce757c42fa07baec10538ebad54
Mentor Palokaj <mentor@palokaj.co>
2020-05-13 15:28:08 +0200

    Enable continuous integration

--- modules/config.js
+++ modules/config.js
@@ -4,15 +4,15 @@ const { normalize } = require( 'path' )
 module.exports = {
 	// Identity variables used in pug templates
 	identity: {
-		title: "Website",
-		desc: "Description of website",
+		title: "Nutshell",
+		desc: "Nutshell - One message a week from people you care about",
 		"logo": "logo.jpg"
 	},
 	// System vars managing some pug elements as well as file paths
 	system: {
 		public: normalize( process.env.NODE_ENV == 'production' ? __dirname + '/../docs/' : __dirname + '/../public/' ),
 		source: normalize( __dirname + '/../src/' ),
-		url: process.env.NODE_ENV == 'production' ? 'https://actuallymentor.github.io/webpack-frontend-only/' : 'http://' + ip.address() + ':3000/',
+		url: process.env.NODE_ENV == 'production' ? 'https://nutshell.social/' : 'http://' + ip.address() + ':3000/',
 		gverification: undefined,
 		timestamp: new Date().getTime(),
 		year: new Date().getFullYear()
@@ -25,7 +25,7 @@ module.exports = {
 		twitter: "@actuallymentor",
 		// facebook profile id, used for retargeting ad permissions
 		facebook: "1299359953416544",
-		url: "https://www.skillcollector.com/"
+		url: "https://nutshell.social/"
 	},
 	// Tracking codes
 	track: {
